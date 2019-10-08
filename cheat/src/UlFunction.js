2459c51337613c31c312d4bfd919411783956fef
Jason Real <tinidian@gmail.com>
2019-10-07 21:54:35 -0600

    Simple ul

---
+++ cheat/src/UlFunction.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function UlFunction() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default UlFunction
