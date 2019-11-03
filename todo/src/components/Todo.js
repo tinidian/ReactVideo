38e3e414fb6b68f28c9402930e5bde6ed2c2f3aa
Jason Real <tinidian@gmail.com>
2019-11-03 12:15:16 -0700

    Video Todo

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todo(){
+    return (
+        <div>
+            <input type="checkbox" name="todo1" value="todo1" /> Todo 1<br/>
+            <input type="checkbox" name="todo2" value="todo2" /> Todo 2<br/>
+            <input type="checkbox" name="todo3" value="todo3" /> Todo 3<br/>
+        </div>
+    )
+}
+
+export default Todo;
---
+++ todo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Todo from "./components/Todo";
+import './style.css';
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
