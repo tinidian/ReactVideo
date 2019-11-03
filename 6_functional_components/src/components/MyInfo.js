55d4136719d4ca96bcade8f1e4807bb20f20b30b
Jason Real <tinidian@gmail.com>
2019-11-03 09:21:00 -0700

    More video practice

---
+++ 6_functional_components/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Jason Real</h1>
+            <p>I really want to learn react!</p>
+            <ul>
+                <li>Antarctica</li>
+                <li>Alaska</li>
+                <li>Brasil</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
